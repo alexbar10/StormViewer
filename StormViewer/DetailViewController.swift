7523be4ce22f3403f352bfdb50e1b17f692d0331
Alejandro Barranco <alejandro.barranco@geoenlace.com>
2019-12-11 12:24:49 -0600

    feat(): Building a detail screen

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Alejandro Barranco on 11/12/19.
+//  Copyright © 2019 alexbar10. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
